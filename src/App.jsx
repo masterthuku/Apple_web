9f9c1eff527ba598426bb83fd1d7b2ce3e9d98fa
masterthuku <peterthuku706@gmail.com>
2024-06-12 19:59:11 +0300

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import Features from "./components/Features"
+import Footer from "./components/Footer"
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
+import HowItWorks from "./components/HowItWorks"
 import Model from "./components/Model"
 import Navbar from "./components/Navbar"
 
@@ -10,6 +13,9 @@ const App = () => {
       <Hero/>
       <Highlights/>
       <Model/>
+      <Features/>
+      <HowItWorks/>
+      <Footer/>
     </main>
   )
 }
--- src/utils/animations.js
+++ src/utils/animations.js
@@ -1,3 +1,20 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export const animateWithGsap = (target, animationProps, scrollProps) => {
+  gsap.to(target, {
+    ...animationProps,
+    scrollTrigger: {
+      trigger: target,
+      toggleActions: "restart reverse restart reverse",
+      start: "top 85%",
+      ...scrollProps,
+    },
+  });
+};
+
 export const animateWithGsapTimeline = (
   timeline,
   rotationRef,
@@ -6,25 +23,27 @@ export const animateWithGsapTimeline = (
   secondTarget,
   animationProps
 ) => {
-    timeline.to(rotationRef.current.rotation, {
-        y: rotationState,
-        duration: 1,
-        ease: "power2.inOut",
-    })
+  timeline.to(rotationRef.current.rotation, {
+    y: rotationState,
+    duration: 1,
+    ease: "power2.inOut",
+  });
 
-    timeline.to(
-        firstTarget, {
-            ...animationProps,
-            ease: "power2.inOut",
-        },
-        '<'
-    )
+  timeline.to(
+    firstTarget,
+    {
+      ...animationProps,
+      ease: "power2.inOut",
+    },
+    "<"
+  );
 
-    timeline.to(
-        secondTarget, {
-            ...animationProps,
-            ease: "power2.inOut",
-        },
-        '<'
-    )
+  timeline.to(
+    secondTarget,
+    {
+      ...animationProps,
+      ease: "power2.inOut",
+    },
+    "<"
+  );
 };
